bf3aa8282189feace6c5f2cc0616064965a27fb5
YunaH <YunaH503@gmail.com>
2020-02-22 17:51:54 -0500

    Added User Auth files
    -all Auth functionality currently working as intended.
    -created the tables folder with api.js, events.js and ui.js

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,91 @@
+'use strict'
+
+const store = require('./../store')
+
+// Sign Up
+const onSignUpSuccess = function (response) {
+  console.log(response)
+  $('#message').removeClass()
+  $('#message').addClass('success-message')
+  $('#message').text(response.user.email + ' successfully signed up!')
+  $('#sign-up').trigger('reset')
+  $('#sign-in').trigger('reset')
+}
+
+const onSignUpFailure = function (response) {
+  console.log(response)
+  $('#message').removeClass()
+  $('#message').addClass('failure-message')
+  $('#message').text('Failed to sign up')
+  $('#sign-up').trigger('reset')
+  $('#sign-in').trigger('reset')
+}
+
+// Sign In
+const onSignInSuccess = function (response) {
+  $('#message').removeClass()
+  $('#message').addClass('success-message')
+  $('#message').text(response.user.email + ' successfully signed in')
+  $('#sign-in').trigger('reset')
+  $('#sign-up').trigger('reset')
+  store.user = response.user
+  $('#sign-out').show()
+  $('#sign-in').hide()
+  $('#sign-up').hide()
+  $('#home-message').hide()
+  $('#change-password').show()
+}
+
+const onSignInFailure = function (response) {
+  $('#message').removeClass()
+  $('#message').addClass('failure-message')
+  $('#message').text('Signed in failed. 😭')
+  $('#sign-in').trigger('reset')
+  $('#sign-up').trigger('reset')
+}
+
+// Change Password
+const onChangePasswordSuccess = function (response) {
+  $('#message').removeClass()
+  $('#message').addClass('success-message')
+  $('#message').text('Changed Password Succeeded!')
+  $('#change-password').trigger('reset')
+}
+
+const onChangePasswordFailure = function (response) {
+  $('#message').removeClass()
+  $('#message').addClass('failure-message')
+  $('#message').text('You can not change your password muahahaha!')
+  $('#change-password').trigger('reset')
+}
+
+// Sign Out
+const onSignOutSuccess = function (response) {
+  $('#message').removeClass()
+  $('#message').addClass('success-message')
+  $('#message').text('Sign Out Succeeded!')
+  $('#change-password').hide()
+  $('#change-password').trigger('reset')
+  $('#sign-out').hide()
+  $('#sign-in').show()
+  $('#sign-up').show()
+  $('#password-button').hide()
+  store.user = null
+}
+
+const onSignOutFailure = function (response) {
+  $('#message').removeClass()
+  $('#message').addClass('failure-message')
+  $('#message').text('Sign Out Failed 😭')
+}
+
+module.exports = {
+  onSignUpSuccess,
+  onSignUpFailure,
+  onSignInSuccess,
+  onSignInFailure,
+  onChangePasswordSuccess,
+  onChangePasswordFailure,
+  onSignOutSuccess,
+  onSignOutFailure
+}
