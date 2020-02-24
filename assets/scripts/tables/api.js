5de3d1361fbbbbc112764101ee6a158372de4f70
YunaH <YunaH503@gmail.com>
2020-02-24 17:43:21 -0500

    Added curl scripts and new js files for tables

--- assets/scripts/tables/api.js
+++ assets/scripts/tables/api.js
@@ -1,1 +1,16 @@
-'user strict'
+'use strict'
+
+const config = require('./../config')
+// const store = require('./../store')
+
+const signIn = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    data: data
+  })
+}
+
+module.exports = {
+  signIn
+}
--- assets/scripts/tables/events.js
+++ assets/scripts/tables/events.js
@@ -1,1 +1,21 @@
-'user strict'
+'use strict'
+const getFormFields = require('./../../../lib/get-form-fields')
+const api = require('./api')
+const ui = require('./ui')
+
+// Sign In function
+const onSignIn = function (event) {
+  event.preventDefault()
+
+  const form = event.target
+  const data = getFormFields(form)
+  // console.log(data)
+
+  api.signIn(data)
+    .then(ui.onSignInSuccess)
+    .catch(ui.onSignInFailure)
+}
+
+module.exports = {
+  onSignIn
+}
