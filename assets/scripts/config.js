9be02c43ef74a370ac1bad674d8419604bc355b1
YunaH <YunaH503@gmail.com>
2020-02-26 18:26:26 -0500

    Edited URL again

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://frozen-eyrie-28018.herokuapp.com',
+  production: 'https://frozen-eyrie-28018.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
